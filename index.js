708c2d03858b6b286f3a85d727dfe57b897bc01e
vanessuniq <vanessuniq@gmail.com>
2021-03-23 21:38:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+    if (myString.length > 1) {
 
+        printString(myString.substring(1, myString.length))
+    }
+};
+
+function reverseString(myString) {
+    const size = myString.length;
+    if (myString.length > 1) {
+        return myString[size - 1] + reverseString(myString.substring(0, size - 1));
+    } else {
+        return myString;
+    }
+};
+
+function isPalindrome(myString) {
+    //return reverseString(myString) === myString;
+    const size = myString.length;
+    if (size === 0 || myString[0] !== myString[size - 1]) {
+        return false
+    };
+    if (size > 1) {
+        isPalindrome(myString.substring(1, size - 1));
+    };
+    return true;
+};
+
+function addUpTo(array, index) {
+
+};
